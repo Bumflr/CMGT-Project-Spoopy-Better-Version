57ab887d3b9588229fafa39b33a2dd59724360a4
Quinten5 <qckuiper@gmail.com>
2023-05-29 16:57:27 +0200

    GrabbingAnimation

--- Assets/Scripts/SC_EnemyAttack.cs
+++ Assets/Scripts/SC_EnemyAttack.cs
@@ -8,6 +8,8 @@ public enum EnemyAttackStates
 }
 public class SC_EnemyAttack : MonoBehaviour
 {
+    Animator animator;
+    int isGrabbingHash;
     [Header("Dependencies")]
     public SC_EnemyLogic enemyLogic;
 
@@ -17,6 +19,14 @@ public class SC_EnemyAttack : MonoBehaviour
     public LayerMask whatIsPlayer;
 
     public bool holdingPlayer;
+
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        isGrabbingHash = Animator.StringToHash("isGrabbing");
+    }
 
     private void Awake()
     {
@@ -52,12 +62,16 @@ public class SC_EnemyAttack : MonoBehaviour
 
                 enemyLogic.SetEnemyState(EnemyStates.HoldingPlayer);
                 //After grabbing the player, set the enemy state to something like grabbing , and stop the enemy of doing anything else  
+
+                animator.SetBool("isGrabbing", true);
             }
         }
     }
 
     public IEnumerator PlayerEscaped()
     {
+        animator.SetBool("isGrabbing", false);
+
         enemyLogic.SetEnemyState(EnemyStates.Stunned);
 
         yield return new WaitForSeconds(1f);
