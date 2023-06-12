79e5fb0ebd48053cc02f068ab2f4460e8a4e3f25
Bumflr <smt25q@gmail.com>
2023-06-12 23:56:30 +0200

    dab

--- Assets/Scripts/UI_GameOver.cs
+++ Assets/Scripts/UI_GameOver.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UI_GameOver : MonoBehaviour
 {
@@ -36,6 +37,11 @@ public class UI_GameOver : MonoBehaviour
     public void RestartGame()
     {
         GameManager.Instance.ReloadScene();
+    }
+
+    public void GameOver()
+    {
+        SceneManager.LoadScene("Title Screen");
     }
 
 }
